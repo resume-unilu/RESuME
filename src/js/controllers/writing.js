4fbdc6e226e6a2c3b9481d7f7a9c4db7db01655d
Michael BREMEC <michael.bremec@rtc4water.com>
2019-09-16 14:39:57 +0200

    Prevent user to remove all the authors

--- src/js/controllers/writing.js
+++ src/js/controllers/writing.js
@@ -177,6 +177,13 @@ angular.module('miller')
       });
     }
 
+    $scope.detachAuthor = function (target) {
+      if ($scope.story.authors.length < 1 && target) {
+        $scope.story.authors.push(target);
+        alert('At least one author is needed')
+      }
+    }
+
     $scope.removeCover = function(doc) {
       $log.debug('WritingCtrl -> removeCover() doc:', doc.id);
       if($scope.isSaving){
