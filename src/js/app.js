cedf8b6abbfea981e553a8f01450eead89bd20e1
mbremec <michael.bremec@rtc4water.com>
2021-04-26 18:10:35 +0200

    Revert cache change

--- src/js/app.js
+++ src/js/app.js
@@ -142,14 +142,6 @@ angular
         }
       };
     });
-
-    // Remove cache
-    if (!$httpProvider.defaults.headers.get) {
-      $httpProvider.defaults.headers.get = {};
-    }
-    $httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
-    $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
-    $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
   })
   .config(function($locationProvider) {
     $locationProvider.html5Mode({
