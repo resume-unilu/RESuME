089fb6ffa4768dce4a0c2e97dcd043ecdf03a6b9
mbremec <michael.bremec@rtc4water.com>
2021-11-15 11:10:03 +0100

    Remove filters panel on archive page

--- src/js/controllers/archive.js
+++ src/js/controllers/archive.js
@@ -2,4 +2,6 @@ angular.module('miller')
   .controller('ArchiveCtrl', function ($scope, $log, $state, $timeout, $q, AuthorFactory, TagFactory, RUNTIME, EVENTS) {
     $scope.rootStatename = 'archives';
     $scope.mainStatename = 'archives.all';
+    $scope.noFiltering = true;
+    $scope.availabileOrderby = [];
   });
